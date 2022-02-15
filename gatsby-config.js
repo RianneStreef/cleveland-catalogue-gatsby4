fd9aab8553a992c6642d5a93e82043f0fcfd6580
Rianne Streef <56265374+RianneStreef@users.noreply.github.com>
2022-02-15 12:22:42 +0100

    dotenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,36 +1,75 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: "https://clevelandgolf-catalogue.app",
+    title: "cleveland-catalogue",
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "YSyuEGIw0xaZqPb7bL_BOx2Iaq69iTTLAJJ-vijZV-c",
-      "spaceId": ""
-    }
-  }, "gatsby-plugin-styled-components", "gatsby-plugin-image", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "G-B9R6TTCD84"
-    }
-  }, "gatsby-plugin-react-helmet", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-transformer-remark", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-CR9V44788C", // Google Analytics / GA
+          "G-LY2284MQMR",
+          "UA-220311142-1",
+        ],
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+        // defaults to false
+        enableWebVitalsTracking: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Cleveland Golf EU & Uk Catalogue",
+        short_name: "Cleveland",
+        start_url: "/",
+        background_color: "#000000",
+        theme_color: "#FFFFFF",
+        display: "fullscreen",
+        icon: "./src/images/icon.png",
+      },
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: ["/"],
+      },
     },
-    __key: "pages"
-  }]
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: process.env.GATSBY_CONTENTFUL_TOKEN,
+        spaceId: process.env.GATSBY_CONTENTFUL_ID,
+      },
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    `gatsby-plugin-image`,
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./src/components/Layout.jsx`),
+      },
+    },
+  ],
 };
